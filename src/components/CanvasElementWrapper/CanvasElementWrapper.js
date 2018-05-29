dbb6837344bacc7253ee253c1b4327edc57b300c
jieun <821jieun@gmail.com>
2018-05-29 17:02:58 -0400

    added touch

--- src/components/CanvasElementWrapper/CanvasElementWrapper.js
+++ src/components/CanvasElementWrapper/CanvasElementWrapper.js
@@ -27,12 +27,42 @@ const mapStateToProps = (state) => {
       this.setCanvasHeight();
       this.screenResize();
       this.updateCanvas();
+      this.mobileTouchListeners();
+    }
+
+    mobileTouchListeners() {
+      const canvas = this.refs.canvas;
+      const ctx = canvas.getContext("2d");
+      const r = 10; // draw radius
+      let lastPt = null;
+      ctx.lineWidth = r * 2;
+      ctx.lineCap = "round";//butt||square
+      ctx.fillStyle = "black";
+
+      canvas.addEventListener("touchmove", draw, false);
+      canvas.addEventListener("touchend", end, false);
+
+      function draw(e) {
+        e.preventDefault();
+        if (lastPt != null) {
+          ctx.beginPath();
+          ctx.moveTo(lastPt.x, lastPt.y);
+          ctx.lineTo(e.touches[0].pageX, e.touches[0].pageY);
+          ctx.stroke();
+        }
+        lastPt = {x: e.touches[0].pageX, y:e.touches[0].pageY};
+      }
+
+      function end(e) {
+        e.preventDefault();
+        lastPt = null;
+      }
     }
 
     screenResize() {
       this.props.dispatch(screenResize(this.props.screenWidth))
     }
-    
+
     setCanvasHeight() {
       let height = window.innerHeight;
       this.setState({
@@ -53,22 +83,20 @@ const mapStateToProps = (state) => {
 
       function yesDraw() { draw = true; lineStart = true }
 
-      // touch
-      function touchMove(e) {
-         const bounds = canvas.getBoundingClientRect();
-         const x = e.pageX - bounds.left - window.scrollX;
-         const y = e.pageY - bounds.top - window.scrollY;
-         // const x = e.pageX - bounds.left - scrollX;
-         // const y = e.pageY - bounds.top - scrollY;
-         if(draw && x > -r && x < canvas.width + r && y > -r && y < canvas.height + r){
-            drawing(x,y);
-         }
-      }
-
-      //touch
-      document.addEventListener("touchmove",touchMove);
-      document.addEventListener("touchdown",yesDraw);
-      document.addEventListener("touchup",noDraw);
+      // // touch
+      // function touchMove(e) {
+      //    const bounds = canvas.getBoundingClientRect();
+      //    const x = e.pageX - bounds.left - window.scrollX;
+      //    const y = e.pageY - bounds.top - window.scrollY;
+      //    if(draw && x > -r && x < canvas.width + r && y > -r && y < canvas.height + r){
+      //       drawing(x,y);
+      //    }
+      // }
+      //
+      // //touch
+      // document.addEventListener("touchmove",touchMove);
+      // document.addEventListener("touchdown",yesDraw);
+      // document.addEventListener("touchup",noDraw);
 
       // mouse
       function mouseMove(e) {
