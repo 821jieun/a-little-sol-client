a049574b49b415a95b865112506a0c2844eec2da
jieun <821jieun@gmail.com>
2018-05-31 09:54:16 -0400

    added tests

--- src/components/CanvasElementWrapper/CanvasElementWrapper.test.js
+++ src/components/CanvasElementWrapper/CanvasElementWrapper.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { shallow, mount } from 'enzyme';
-import { shallowWithStore } from 'enzyme-redux';
+import { shallowWithStore, mountWithStore, mountWithState } from 'enzyme-redux';
 import { createMockStore } from 'redux-test-utils';
 import {saveCanvasToGallery} from '../../actions';
 
@@ -29,9 +29,79 @@ describe('<CanvasElementWrapper />', () => {
       const ConnectedComponent = connect(mapStateToProps)(ReactComponent);
       const component = shallowWithStore(<ConnectedComponent />, createMockStore(expectedState));
       expect((component).length).toEqual(1);
-      // expect(component).to.have.property('length', 1);
     });
 
   });
   //
+// describe('example mountWithState', () => {
+//     const ReactComponent = () => (<CanvasElementWrapper />);
+//   it('works', () => {
+//     const expectedState = 'expectedState';
+//     const mapStateToProps = (state) => ({
+//       state,
+//     });
+//     const ConnectedComponent = connect(mapStateToProps)(ReactComponent);
+//     const selectedInstructionText = 'test';
+//     const component = mountWithState(<ConnectedComponent />, expectedState).dive();
+//     expect(component.props().state).toBe(expectedState);
+//   });
+// });
+  // describe('dispatch', () => {
+  //   const ReactComponent = () => (<CanvasElementWrapper />);
+  //
+  //     it('works', () => {
+  //       const action = {
+  //         type: 'type',
+  //       };
+  //       const mapDispatchToProps = (dispatch) => ({
+  //         dispatchProp() {
+  //           dispatch(action);
+  //         },
+  //       });
+  //       const store = createMockStore();
+  //
+  //       const ConnectedComponent = connect(undefined, mapDispatchToProps)(ReactComponent);
+  //       const component = shallow(<ConnectedComponent />, store).dive();
+  //       component.props().dispatchProp();
+  //       expect(store.isActionDispatched(action)).toBe(true);
+  //     });
+  //   });
+  // // describe('dispatch', () => {
+  //   const ReactComponent = () => (<CanvasElementWrapper />);
+  //
+  //     it('works', () => {
+  //       const action = {
+  //         type: 'type',
+  //       };
+  //       const mapDispatchToProps = (dispatch) => ({
+  //         dispatchProp() {
+  //           dispatch(action);
+  //         },
+  //       });
+  //       const store = createMockStore();
+  //
+  //       const ConnectedComponent = connect(undefined, mapDispatchToProps)(ReactComponent);
+  //       const component = mountWithStore(<ConnectedComponent saveCanvasToGallery={saveCanvasToGallery('selectedInstructionText')}/>, store);
+  //       component.props().dispatchProp();
+  //       expect(store.isActionDispatched(action)).toBe(true);
+  //     });
+  //   });
+
+  // describe('callback', () => {
+  //   it('Should handle click', () => {
+  //     const expectedState = 'expectedState';
+  //     const mapStateToProps = (state) => ({
+  //       state,
+  //     });
+  //     const ConnectedComponent = connect(mapStateToProps)(ReactComponent);
+  //     const callback = jest.fn();
+  //     const component = mountWithStore(<ConnectedComponent handleClick={callback}/>, createMockStore(expectedState));
+  //
+  //     component.find('save-button').simulate('click')
+  //
+  //     expect(callback).toHaveBeenCalled();
+  //   });
+  //
+  // });
+
 });
