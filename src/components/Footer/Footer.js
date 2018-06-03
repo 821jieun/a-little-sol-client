bc731de7ff82f2448a3ff5278c4356bba2cf16e7
jieun <821jieun@gmail.com>
2018-06-03 15:01:27 -0400

    visited link color change

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ export default class Footer extends React.Component {
     return (
       <div className="footer">
         <ul className="footer-links">
-          <p><a className="footer-more-info-sol" href="https://fraenkelgallery.com/tag/sol-lewitt" target="_blank" alt="a link to more information about sol lewitt">learn more about sol lewitt</a></p>
+          <p><a className="footer-more-info-sol" rel="noopener noreferrer" href="https://fraenkelgallery.com/tag/sol-lewitt" target="_blank" alt="a link to more information about sol lewitt">learn more about sol lewitt</a></p>
         </ul>
       </div>
     )
