29679754c94471c350f68520a07a4af68563f14d
jieun <821jieun@gmail.com>
2018-06-04 15:27:38 -0400

    redux-thunk

--- src/store.js
+++ src/store.js
@@ -2,11 +2,8 @@ import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './reducers/rootReducer';
 
 import {createLogger} from 'redux-logger';
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
 
-// import {loadAuthToken} from './local-storage';
-//
-// import {setAuthToken} from './actions/index';
 
 export default createStore(
     rootReducer,
