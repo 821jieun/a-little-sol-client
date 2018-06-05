9eb09fb214331a998bcb86e12d204279d76b3938
jieun <821jieun@gmail.com>
2018-06-05 12:41:53 -0400

    fixed url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // export const API_BASE_URL =
 //     process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'https://salty-lowlands-52211.herokuapp.com/';
+    process.env.REACT_APP_API_BASE_URL || 'https://salty-lowlands-52211.herokuapp.com';
